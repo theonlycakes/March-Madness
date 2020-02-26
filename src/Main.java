6ba1161be64b96f1e9551921cd32604354e84dec
Peter Spychalsky <23511567+theonlycakes@users.noreply.github.com>
2020-02-26 13:36:26 -0500

    Add files via upload

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 import java.awt.*;
+import java.awt.event.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Frame f = new Frame();
         f.setSize(400,400);
+        f.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
         f.show();
     }
 }
