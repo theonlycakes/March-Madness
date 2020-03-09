e33bed5ee723a410e9d8d2a5e7b2931e912d7804
Peter Spychalsky <23511567+theonlycakes@users.noreply.github.com>
2020-03-09 11:25:20 -0400

    stuff

---
+++ src/com/company/players.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class players {
+    public int hits;
+    public int total;
+    public int misses;
+    public int score;
+    public players(){
+        score = 0;
+        misses = 0;
+        total = 0;
+        hits = 0;
+    }
+
+}
---
+++ src/com/company/playersToTeams.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+public class playersToTeams {
+    public playersToTeams(ArrayList teamList) {
+        //Stores a map for each team which contains objects for each player
+        Map<String, Map<String, players>> teams = new HashMap<String, Map<String, players>>();
+
+    }
+}
